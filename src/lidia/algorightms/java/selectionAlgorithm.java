e022e94c78e1dace386a7c37a010e2cae01d5f41
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-18 14:45:04 +0300

    continue to work on the algorithm

--- src/lidia/algorightms/java/selectionAlgorithm.java
+++ src/lidia/algorightms/java/selectionAlgorithm.java
@@ -5,32 +5,41 @@ import java.util.Arrays;
 public class selectionAlgorithm {
     public static void main(String[] args){
         System.out.println("hello");
-        int[] array = {4, 2, 3, 6, 7, 9};
-        int[] result = selectSort(array);
-        System.out.print(result);
+        int[] anArray = {
+                4, 2, 3, 6, 7, 9
+        };
+        System.out.println(Arrays.toString(anArray));
+        int[] result = selectSort(anArray);
+        System.out.print(Arrays.toString(result));
     }
 
     public static int[] selectSort(int[] array) {
         assert array != null : "failed precondition";
         int temporaryHolder;
         for (int i=0; i < array.length; ++i) {
-            int[] newArray = Arrays.copyOfRange(array, i, array.length-1);
+            int[] newArray = Arrays.copyOfRange(array, i, array.length);
+            System.out.println(Arrays.toString(newArray));
             int minValueIndex = getMinValueIndex(newArray);
             System.out.println(minValueIndex);
             temporaryHolder = array[i];
+            System.out.println("tempHold: " + temporaryHolder);
             array[i] = array[minValueIndex];
+            System.out.println("firstVal: " + array[i]);
             array[minValueIndex] = temporaryHolder;
+            System.out.println(Arrays.toString(array));
         }
     return array;
     }
 
     public static int getMinValueIndex(int[] array) {
         int minValue = Integer.MAX_VALUE;
+        System.out.println(minValue);
         int minValueIndex = 0;
         for (int i = 0; i < array.length; ++i) {
             if (array[i] < minValue) {
                 minValue = array[i];
                 minValueIndex = i;
+                System.out.println(minValue + "and" + minValueIndex);
             }
         }
         return minValueIndex;

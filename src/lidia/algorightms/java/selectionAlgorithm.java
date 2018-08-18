3a93962f69cad31459a9dc218749e724d8aed3f1
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-18 10:52:56 +0300

    initial commit for the selection sort algorithm in solving process

---
+++ src/lidia/algorightms/java/selectionAlgorithm.java
@@ -0,0 +1,18 @@
+package lidia.algorightms.java;
+
+public class selectionAlgorithm {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+
+    public static int[] selectSort(int[] array) {
+        assert array != null : "failed precondition";
+        int minIndex = 0;
+        int temporaryHolder = 0;
+        for (int i : array) {
+            int minValue = getMinValue
+        }
+
+
+    }
+}

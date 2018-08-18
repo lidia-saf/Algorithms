806c705bfc0a40f1ff37e186f0797dbb015bc1f1
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-18 15:52:54 +0300

    tried with two more arrays

--- src/lidia/algorightms/java/selectionAlgorithm.java
+++ src/lidia/algorightms/java/selectionAlgorithm.java
@@ -4,13 +4,21 @@ import java.util.Arrays;
 
 public class selectionAlgorithm {
     public static void main(String[] args){
-        System.out.println("hello");
         int[] anArray = {
                 4, 2, 3, 6, 7, 9
         };
-        System.out.println(Arrays.toString(anArray));
+        int[] arrayTwo = {
+                5, 10, 35, 2, 8, 7, 1, 4
+        };
+        int[] arrayThree = {
+                78, 15, -5, 3, 90
+        };
         int[] result = selectSort(anArray);
-        System.out.print(Arrays.toString(result));
+        int[] resultTwo = selectSort(arrayTwo);
+        int[] resultThree = selectSort(arrayThree);
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(resultTwo));
+        System.out.println(Arrays.toString(resultThree));
     }
 
     public static int[] selectSort(int[] array) {
@@ -18,7 +26,6 @@ public class selectionAlgorithm {
         int temporaryHolder;
         for (int i=0; i < array.length; ++i) {
             int[] newArray = Arrays.copyOfRange(array, i, array.length);
-            System.out.println(Arrays.toString(newArray));
             int minValue = getMinValue(newArray);
             int minValueIndex = 0;
             for (int j = 0; j < array.length; ++j) {
@@ -26,24 +33,18 @@ public class selectionAlgorithm {
                     minValueIndex = j;
                 }
             }
-            System.out.println(minValue + "and" + minValueIndex);
             temporaryHolder = array[i];
-            System.out.println("tempHold: " + temporaryHolder);
             array[i] = array[minValueIndex];
-            System.out.println("firstVal: " + array[i]);
             array[minValueIndex] = temporaryHolder;
-            System.out.println(Arrays.toString(array));
         }
     return array;
     }
 
     public static int getMinValue(int[] array) {
         int minValue = Integer.MAX_VALUE;
-        System.out.println(minValue);
         for (int i = 0; i < array.length; ++i) {
             if (array[i] < minValue) {
                 minValue = array[i];
-                System.out.println(minValue);
             }
         }
         return minValue;

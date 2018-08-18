7b77ce8e6e2f8ed3e63b369367ea1c9d919cb429
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-18 12:42:51 +0300

    catched illegal argument exception

--- src/lidia/algorightms/java/selectionAlgorithm.java
+++ src/lidia/algorightms/java/selectionAlgorithm.java
@@ -1,18 +1,37 @@
 package lidia.algorightms.java;
 
+import java.util.Arrays;
+
 public class selectionAlgorithm {
     public static void main(String[] args){
         System.out.println("hello");
+        int[] array = {4, 2, 3, 6, 7, 9};
+        System.out.println(selectSort(array));
     }
 
     public static int[] selectSort(int[] array) {
         assert array != null : "failed precondition";
-        int minIndex = 0;
         int temporaryHolder = 0;
         for (int i : array) {
-            int minValue = getMinValue
+            int[] newArray = Arrays.copyOfRange(array, i, array.length -1);
+            int minValueIndex = getMinValueIndex(newArray);
+            System.out.println(minValueIndex);
+            temporaryHolder = array[i];
+            array[i] = array[minValueIndex];
+            array[minValueIndex] = temporaryHolder;
         }
+    return array;
+    }
 
-
+    public static int getMinValueIndex(int[] array) {
+        int minValue = array[0];
+        int minValueIndex = 0;
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minValueIndex = i;
+            }
+        }
+        return minValueIndex;
     }
 }

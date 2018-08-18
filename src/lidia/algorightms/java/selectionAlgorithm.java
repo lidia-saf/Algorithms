4c2008908334572afc0a8042cd94d14309503322
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-18 14:16:36 +0300

    received weird output

--- src/lidia/algorightms/java/selectionAlgorithm.java
+++ src/lidia/algorightms/java/selectionAlgorithm.java
@@ -6,14 +6,15 @@ public class selectionAlgorithm {
     public static void main(String[] args){
         System.out.println("hello");
         int[] array = {4, 2, 3, 6, 7, 9};
-        System.out.println(selectSort(array));
+        int[] result = selectSort(array);
+        System.out.print(result);
     }
 
     public static int[] selectSort(int[] array) {
         assert array != null : "failed precondition";
-        int temporaryHolder = 0;
-        for (int i : array) {
-            int[] newArray = Arrays.copyOfRange(array, i, array.length -1);
+        int temporaryHolder;
+        for (int i=0; i < array.length; ++i) {
+            int[] newArray = Arrays.copyOfRange(array, i, array.length-1);
             int minValueIndex = getMinValueIndex(newArray);
             System.out.println(minValueIndex);
             temporaryHolder = array[i];
@@ -24,9 +25,9 @@ public class selectionAlgorithm {
     }
 
     public static int getMinValueIndex(int[] array) {
-        int minValue = array[0];
+        int minValue = Integer.MAX_VALUE;
         int minValueIndex = 0;
-        for (int i = 1; i < array.length; ++i) {
+        for (int i = 0; i < array.length; ++i) {
             if (array[i] < minValue) {
                 minValue = array[i];
                 minValueIndex = i;
